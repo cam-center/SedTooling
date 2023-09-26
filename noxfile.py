a2fd37468e0fc2665e23594683473c1be5ad05d7
Jim Schaff <schaff.jim@gmail.com>
2023-09-26 18:36:06 -0400

    add ci.yml for lint/test/coverage

---
+++ noxfile.py
@@ -0,0 +1,30 @@
+import nox
+from nox_poetry import Session, session
+
+nox.options.sessions = ["test", "coverage", "lint"]
+
+
+@session(python=["3.8", "3.9", "3.10", "3.11"])
+def test(s: Session):
+    s.install(".", "pytest", "pytest-cov", "pytest-timeout")
+    s.env["COVERAGE_FILE"] = f".coverage.{s.python}"
+    s.run("python", "-m", "pytest", "--cov", "sed_converter")
+
+
+@session(venv_backend="none")
+def coverage(s: Session):
+    s.run("coverage", "combine")
+    s.run("coverage", "html")
+    s.run("coverage", "xml", "--fail-under=100")
+
+
+@session(venv_backend="none")
+def fmt(s: Session) -> None:
+    s.run("ruff", "check", ".", "--select", "I", "--fix")
+    s.run("black", ".")
+
+
+@session(venv_backend="none")
+def lint(s: Session) -> None:
+    s.run("black", "--check", ".")
+    s.run("ruff", "check", ".")
