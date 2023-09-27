87efdde7235d6af81be7ec465fe8d946da21c961
Jim Schaff <schaff.jim@gmail.com>
2023-09-27 11:41:54 -0400

    Logan's changes

--- src/sed_converter/sedml/sedml_core.py
+++ src/sed_converter/sedml/sedml_core.py
@@ -3,6 +3,11 @@ from sed_converter.sedml.sedml_document import SedMLDocument
 
 
 class SedMLCore:
+    def __int__(self, sedml_files: list[str]):
+        self.files: dict[str, SedMLDocument] = {}
+        for file in sedml_files:
+            self.files[file] = SedMLDocument(file)
+
     def convert_to_sed(self, file_path: str) -> SedDocument:
         sedml = SedMLDocument(file_path)
         return sedml.export_to_sed()
--- src/sed_converter/sedml/sedml_document.py
+++ src/sed_converter/sedml/sedml_document.py
@@ -7,12 +7,18 @@ from libsedml import SedDocument as SedMLDoc
 from libsedml import SedError as SedMLError
 from libsedml import SedModel as SedMLModel
 from libsedml import SedOutput as SedMLOutput
+from libsedml import SedParameter as SedMLParameter
 from libsedml import SedPlot as SedMLPlot
 from libsedml import SedPlot2D as SedMLPlot2D
 from libsedml import SedPlot3D as SedMLPlot3D
+from libsedml import SedRepeatedTask as SedMLRepeatedTask
 from libsedml import SedReport as SedMLReport
 from libsedml import SedSimulation as SedMLSimulation
+from libsedml import SedSubTask as SedMLSubTask
 from libsedml import SedSurface as SedMLSurface
+from libsedml import SedTask as SedMLTask
+from libsedml import SedVariable as SedMLVariable
+from libsedml import XMLNamespaces
 
 
 class SedMLDocument:
@@ -28,6 +34,8 @@ class SedMLDocument:
             raise RuntimeError(exception_message)
 
         # Start pulling from SedML
+        self.variables_set: set[SedMLVariable] = set()
+        self.parameter_set: set[SedMLParameter] = set()
         self.model_dict: dict[str, SedMLModel] = {}
         self.simulation_dict: dict[str, SedMLSimulation] = {}
         self.task_dict: dict[str, SedMLAbstractTask] = {}
@@ -36,47 +44,94 @@ class SedMLDocument:
             self.sedml.getOutput(output_index)
             for output_index in range(self.sedml.getNumOutputs())
         ]
+
+        # Start basic parsing
         self._process_document()
 
     def _process_document(self):
+        #  Each call grabs the needed values for the next "call" until we have parsed models and sims.
         self._process_outputs()
+        self._process_data_gens()
+        self._process_variables_and_params()
+        self._process_tasks()
 
     def _process_outputs(self):
         needed_data_gen_ids: set[str] = set()
         for output in self.output_list:
             if isinstance(output, SedMLPlot):
-                plot: SedMLPlot = output
-                if isinstance(plot, SedMLPlot2D):
-                    plot2: SedMLPlot2D = plot
-                    for i in range(plot2.getNumCurves()):
-                        curve: SedMLCurve = plot2.getCurve(i)
+                if isinstance(output, SedMLPlot2D):
+                    for i in range(output.getNumCurves()):
+                        curve: SedMLCurve = output.getCurve(i)
                         needed_data_gen_ids.add(curve.getXDataReference())
                         needed_data_gen_ids.add(curve.getYDataReference())
-                if isinstance(plot, SedMLPlot3D):
-                    plot3: SedMLPlot3D = plot
-                    for i in range(plot3.getNumSurfaces()):
-                        surface: SedMLSurface = plot3.getSurface(i)
+                if isinstance(output, SedMLPlot3D):
+                    for i in range(output.getNumSurfaces()):
+                        surface: SedMLSurface = output.getSurface(i)
                         needed_data_gen_ids.add(surface.getXDataReference())
                         needed_data_gen_ids.add(surface.getYDataReference())
                         needed_data_gen_ids.add(surface.getZDataReference())
             if isinstance(output, SedMLReport):
-                report: SedMLReport = output
-                for i in report.getNumDataSets():
-                    dataset: SedMLDataSet = report.getDataSet(i)
+                for i in output.getNumDataSets():
+                    dataset: SedMLDataSet = output.getDataSet(i)
                     needed_data_gen_ids.add(dataset.getDataReference())
 
-        for data_gen in [
-            self.sedml.getDataGenerator(i) for i in self.sedml.getNumDataGenerators()
-        ]:
+        for data_gen in [self.sedml.getDataGenerator(i)
+                         for i in self.sedml.getNumDataGenerators()]:
             data_gen: SedMLDataGenerator = data_gen
             data_gen_id: str = data_gen.getId()
             if data_gen_id in needed_data_gen_ids:
                 self.data_gen_dict[data_gen_id] = data_gen
 
     def _process_data_gens(self):
-        data_gens = list(self.data_gen_dict.values())
-        for data_gen in data_gens:
-            data_gen
+        for data_gen in list(self.data_gen_dict.values()):
+            [self.variables_set.add(data_gen.getVariable(i)) for i in data_gen.getNumVariables()]
+            [self.parameter_set.add(data_gen.getParameter(i)) for i in data_gen.getNumParameters()]
+
+    def _process_variables_and_params(self):
+        task: SedMLAbstractTask
+        needed_task_ids: set[str] = set()
+        [needed_task_ids.add(variable.getTaskReference()) for variable in self.variables_set]
+        [needed_task_ids.add(parameter.getTaskReference()) for parameter in self.variables_set]
+        for task in [self.sedml.getTask(i) for i in range(0, self.sedml.getNumTasks())]:
+            if task.getId() in needed_task_ids:
+                self.task_dict[task.getId()] = task
+
+    def _process_tasks(self):
+        needed_simulation_ids: set[str] = set()
+        needed_model_ids: set[str] = set()
+        for task in list(self.task_dict.values()):
+            if isinstance(task, SedMLTask):
+                needed_model_ids.add(task.getModelReference())
+                needed_simulation_ids.add(task.getSimulationReference())
+            if isinstance(task, SedMLRepeatedTask):
+                needed_model_ids, needed_simulation_ids = self.__delve_into_repeated_task(task)
+        model: SedMLModel
+        sim: SedMLSimulation
+        for model in [self.sedml.getModel(i) for i in self.sedml.getNumModels()]:
+            self.model_dict[model.getId()] = model
+        for sim in [self.sedml.getSimulation(i) for i in self.sedml.getNumSimulations()]:
+            self.simulation_dict[sim.getId()] = sim
+
+    def __delve_into_repeated_task(self, repeated_task: SedMLRepeatedTask) \
+            -> tuple[set[SedMLModel], set[SedMLSimulation]]:
+
+        model_set: set[SedMLModel] = set()
+        sim_set: set[SedMLSimulation] = set()
+        subtask: SedMLSubTask
+        for subtask in [repeated_task.getSubTask(i) for i in repeated_task.getNumSubTasks()]:
+            target_task = self.sedml.getTask(subtask.getTask())  # get task by id
+            if isinstance(target_task, SedMLRepeatedTask):
+                ret_models, ret_sims = self.__delve_into_repeated_task(target_task)
+                model_set.update(ret_models)
+                sim_set.update(ret_sims)
+            elif isinstance(target_task, SedMLTask):
+                model_set.add(target_task.getModelReference())
+                sim_set.add(target_task.getSimulationReference())
+        return model_set, sim_set
 
     def export_to_sed(self):
-        pass
+        # Start with Namespaces
+        xmlns: XMLNamespaces = self.sedml.getNamespaces()
+        for namespace in [xmlns.getPrefix(i) for i in range(0, xmlns.getNumNamespaces())]:
+            print(namespace)
+
